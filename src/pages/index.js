69ba79fb92be489c8e3d77cd21e90e5d9ae5c0ab
kunal <kunal.dahat@aiab.in>
2023-08-05 01:43:36 +0530

    finall

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,106 +15,69 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Name - Kunal Dahat (Cloud/Devops Engineer)
-            {/* <code className={styles.code}>src/pages/index.js</code> */}
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {/* By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority */}
-              {/* /> */}
-            </a>
-          </div>
-        </div>
+      <div style={{ fontSize: '36px' }}>
+       <p>
+        Name - Kunal Dahat (Cloud/Devops Engineer)
+        {/* <code>src/pages/index.js</code> */}
+       </p>
+      </div>
 
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            
-            <Image
-              src="/thirteen.svg"
-              alt="Resume"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
+        
 
         <div className={styles.grid}>
           <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href="https://drive.google.com/file/d/12cBVH8A1t7oUOL37xOQbzlqFjPC2OcX-/view?usp=sharing"
             className={styles.card}
             target="_blank"
             rel="noopener noreferrer"
           >
             <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
+              Resume <span>-&gt;</span>
             </h2>
             <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
+              Click to see my awsome resume
             </p>
           </a>
 
           <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            // href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
             target="_blank"
             rel="noopener noreferrer"
           >
             <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
+              Skills <span>-&gt;</span>
             </h2>
             <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
+              Kubernetes, Docker, Linux, Git, Ansible, Nginx ,AWS ,GCP, Azure, Grafana, DNS, CICD, SES, LB, S3, VPC, SSL
             </p>
           </a>
 
           <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href="https://www.linkedin.com/in/kunal-dahat"
             className={styles.card}
             target="_blank"
             rel="noopener noreferrer"
           >
             <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
+              LinkedIn <span>-&gt;</span>
             </h2>
             <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
+              
             </p>
           </a>
 
           <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            // href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
             target="_blank"
             rel="noopener noreferrer"
           >
             <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
+              Deployments <span>-&gt;</span>
             </h2>
             <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
+              Have deployed 50+ UAT deployment on kubernetes via CICD & 10+ Projects Prod deployed
             </p>
           </a>
         </div>
